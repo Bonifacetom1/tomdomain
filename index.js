7cbf757846ebd5407a005c484b081dd9a403d9f8
Bonifacetom1 <bonifacetom1@gmail.com>
2022-09-11 15:31:15 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+function mobileToggle() {
+  var element = document.getElementById("navigation");
+  element.classList.toggle("show");
+}
+
+// Get the container element
+var btnContainer = document.getElementById("myDIV");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    // Add the active class to the current/clicked button
+    this.className += " active";
+  });
+}
+
+
+const authorsEl = document.querySelectorAll('.author');
+const container = document.querySelector('.testimonials-container');
+const nameEl = document.querySelector('.name');
+const textEl = document.querySelector('.text');
+
+const testimonials = [{
+	text: '"First, Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis."',
+	name: 'Christ Jennifer',
+	color: '#00C9A7'
+},{
+	text: '"Second, Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis."',
+	name: 'James',
+	color: '#FF676D'
+},{
+	text: '"Third, Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis."',
+	name: 'Fransis Merry',
+	color: '#9D50BB'
+},{
+	text: '"Fourth, Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis."',
+	name: 'Kumaran',
+	color: '#F16529'
+},{
+	text: '"Fifth, Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis."',
+	name: 'Afrah Khan',
+	color: '#021B79'
+}];
+
+addTestimonial(0);
+
+authorsEl.forEach((author, idx) => {
+	author.addEventListener('click', (e) => {
+		addTestimonial(idx);
+		author.classList.add('selected');
+	})
+});
+
+function addTestimonial(idx) {
+	const testimonial = testimonials[idx];
+	
+	nameEl.innerHTML = testimonial.name;
+	textEl.innerHTML = testimonial.text;
+	container.style.background = testimonial.color;
+	container.style.boxShadow = '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)';
+	
+	authorsEl.forEach(author => {
+		author.classList.remove('selected');
+	});
+}
